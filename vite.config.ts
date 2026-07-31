7feebaa2547119b13ffcd7b1d05c564bcf8ff0f5
Max Gonzalez <52800035+MaxKeenti@users.noreply.github.com>
2026-07-30 20:26:01 -0600

    Add static Sites deployment output

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -9,7 +9,11 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'dist/static',
+				assets: 'dist/static',
+				strict: true
+			})
 		})
 	]
 });

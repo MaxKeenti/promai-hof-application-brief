6ad472a88a67d06caab9ced817635cbeb67bcab5
Max Gonzalez <52800035+MaxKeenti@users.noreply.github.com>
2026-07-30 20:39:23 -0600

    Restore Vercel deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -9,11 +9,7 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter({
-				pages: 'dist/static',
-				assets: 'dist/static',
-				strict: true
-			})
+			adapter: adapter()
 		})
 	]
 });
